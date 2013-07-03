a1ce39d51017b4a40993fd9d5e37e49015e9c0d9
jzienkiewicz <j.zienkiewicz12@imperial.ac.uk>
2013-07-03 18:38:38 +0100

    Fixed EOF. Added aiMaterial.

--- kangaroo/extra/AssimpMissing.h
+++ kangaroo/extra/AssimpMissing.h
@@ -2,64 +2,77 @@
 
 // ------------------------------------------------------------------------------------------------
 aiScene::aiScene()
-	: mFlags()
-	, mRootNode()
-	, mNumMeshes()
-	, mMeshes()
-	, mNumMaterials()
-	, mMaterials()
-	, mNumAnimations()
-	, mAnimations()
-	, mNumTextures()
-	, mTextures()
-	, mNumLights()
-	, mLights()
-	, mNumCameras()
-	, mCameras()
-//    , mPrivate(new Assimp::ScenePrivateData())
+    : mFlags()
+    , mRootNode()
+    , mNumMeshes()
+    , mMeshes()
+    , mNumMaterials()
+    , mMaterials()
+    , mNumAnimations()
+    , mAnimations()
+    , mNumTextures()
+    , mTextures()
+    , mNumLights()
+    , mLights()
+    , mNumCameras()
+    , mCameras()
+    //, mPrivate(new Assimp::ScenePrivateData())
     , mPrivate(0)
-    {
-	}
+{
+}
 
 // ------------------------------------------------------------------------------------------------
 aiScene::~aiScene()
 {
-	// delete all sub-objects recursively
-	delete mRootNode;
+    // delete all sub-objects recursively
+    delete mRootNode;
 
-	// To make sure we won't crash if the data is invalid it's
-	// much better to check whether both mNumXXX and mXXX are
-	// valid instead of relying on just one of them.
-	if (mNumMeshes && mMeshes) 
-		for( unsigned int a = 0; a < mNumMeshes; a++)
-			delete mMeshes[a];
-	delete [] mMeshes;
+    // To make sure we won't crash if the data is invalid it's
+    // much better to check whether both mNumXXX and mXXX are
+    // valid instead of relying on just one of them.
+    if (mNumMeshes && mMeshes)
+        for( unsigned int a = 0; a < mNumMeshes; a++)
+            delete mMeshes[a];
+    delete [] mMeshes;
 
-	if (mNumMaterials && mMaterials) 
-		for( unsigned int a = 0; a < mNumMaterials; a++)
-			delete mMaterials[a];
-	delete [] mMaterials;
+    if (mNumMaterials && mMaterials)
+        for( unsigned int a = 0; a < mNumMaterials; a++)
+            delete mMaterials[a];
+    delete [] mMaterials;
 
-	if (mNumAnimations && mAnimations) 
-		for( unsigned int a = 0; a < mNumAnimations; a++)
-			delete mAnimations[a];
-	delete [] mAnimations;
+    if (mNumAnimations && mAnimations)
+        for( unsigned int a = 0; a < mNumAnimations; a++)
+            delete mAnimations[a];
+    delete [] mAnimations;
 
-	if (mNumTextures && mTextures) 
-		for( unsigned int a = 0; a < mNumTextures; a++)
-			delete mTextures[a];
-	delete [] mTextures;
+    if (mNumTextures && mTextures)
+        for( unsigned int a = 0; a < mNumTextures; a++)
+            delete mTextures[a];
+    delete [] mTextures;
 
-	if (mNumLights && mLights) 
-		for( unsigned int a = 0; a < mNumLights; a++)
-			delete mLights[a];
-	delete [] mLights;
+    if (mNumLights && mLights)
+        for( unsigned int a = 0; a < mNumLights; a++)
+            delete mLights[a];
+    delete [] mLights;
 
-	if (mNumCameras && mCameras) 
-		for( unsigned int a = 0; a < mNumCameras; a++)
-			delete mCameras[a];
-	delete [] mCameras;
+    if (mNumCameras && mCameras)
+        for( unsigned int a = 0; a < mNumCameras; a++)
+            delete mCameras[a];
+    delete [] mCameras;
 
-//	delete static_cast<Assimp::ScenePrivateData*>( mPrivate );
+    //	delete static_cast<Assimp::ScenePrivateData*>( mPrivate );
 }
 
+aiMaterial::~aiMaterial()
+{
+    //Clear();
+    delete[] mProperties;
+}
+
+aiMaterial::aiMaterial()
+{
+    // Allocate 5 entries by default
+    mNumProperties = 0;
+    mNumAllocated = 5;
+    mProperties = new aiMaterialProperty*[5];
+}
