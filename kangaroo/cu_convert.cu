917b79276c603b43dbc68811a96521982f266e59
jzienkiewicz <j.zienkiewicz12@imperial.ac.uk>
2014-03-18 12:43:17 +0000

    Explicit conversion form uchar to float.

--- kangaroo/cu_convert.cu
+++ kangaroo/cu_convert.cu
@@ -30,6 +30,7 @@ void ConvertImage(Image<To> dOut, const Image<Ti> dIn)
 }
 
 // Explicit instantiation
+template void ConvertImage<unsigned char,float>(Image<unsigned char>, const Image<float>);
 template void ConvertImage<float,unsigned char>(Image<float>, const Image<unsigned char>);
 template void ConvertImage<float,unsigned short>(Image<float>, const Image<unsigned short>);
 template void ConvertImage<float,char>(Image<float>, const Image<char>);
